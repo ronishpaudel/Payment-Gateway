3f071fa61835c79ec9d93f5edeb5eb3a5c50fde7
robuGdada <paudelronish@gmail.com>
2024-11-25 09:10:38 +0545

    responsiveness fix

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import PaymentMethods from "@/components/payment-method";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-2 md:p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm">
         <h1 className="text-4xl font-bold mb-8 text-center">Payment Gateway</h1>
         <PaymentMethods />
